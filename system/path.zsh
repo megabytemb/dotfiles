1cf8ad6e33af0e812f2390acaf0231b96dffacbb
Geoffrey Huntley <ghuntley@ghuntley.com>
2011-04-08 04:23:54 +1000

    added nodejs to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/node/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
