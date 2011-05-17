40793b30d6e3be24cbd41030b22e672bc7142913
Geoffrey Huntley <ghuntley@ghuntley.com>
2011-05-17 17:22:17 +1000

    wherz the gems?!

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/node/bin"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/node/bin:/var/lib/gems/1.8/bin"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/var/lib/gems/1.8/man:$MANPATH"
