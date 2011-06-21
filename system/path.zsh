8daebb6c58efb7f59d881591265e70bf476a7e0e
Geoffrey Huntley <ghuntley@ghuntley.com>
2011-06-21 16:37:45 +1000

    added coffee-script (from source) to env path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/node/bin:/var/lib/gems/1.8/bin"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/opt/node/bin:/var/lib/gems/1.8/bin:/opt/coffee-script/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/var/lib/gems/1.8/man:$MANPATH"
