a8c8407b3970a3ad5d7496d2900726a4b480bb33
Geoffrey Huntley <ghuntley@ghuntley.com>
2012-01-04 19:16:04 +1100

    tmuxinator

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,2 +1,6 @@
 # http://blog.oldworld.fr/index.php?post/2010/03/21/256-colors-terminal-with-tmux-and-urxvt
 alias tmux="tmux -2"
+
+# tmuxinator
+[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
+
