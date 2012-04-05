717a34b3f2c27ccd82e1d7aca7c36821b6f37847
Geoffrey Huntley <ghuntley@ghuntley.com>
2012-04-05 15:28:39 +1000

    /s/ghuntley/pbwh/g

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -39,16 +39,11 @@ function s() {
     fi
 }
 
-# ./vmconvert.doit?now=thankyou
-alias rvmc="nohup rdesktop -xl -P -z -0 -g1024x700 -u Administrator vmconverter >/dev/null 2>&1 &"
-alias rmwa="nohup rdesktop -xl -P -z -0 -g1024x700 -u Administrator mwa >/dev/null 2>&1 &"
-alias rtunes="nohup rdesktop -xl -P -z -0 -g1024x700 -u ghuntley music >/dev/null 2>&1 &"
-
 # rip cd
 alias ripcd="abcde -a cddb,read,encode,tag,move,playlist,clean -d /dev/cdrom -o m4a -V -x"
 
 # poor mans vpn
-alias svpn="spwd;cd ~/bin/sshuttle && git reset --hard && git pull && ./sshuttle --dns -vvr ghuntley@ghuntley.com 0/0; lpwd"
+alias svpn="spwd;cd ~/bin/sshuttle && git reset --hard && git pull && ./sshuttle --dns -vvr ghuntley@pbwh.org 0/0; lpwd"
 
 # youtube-dl
 alias youtube='youtube-dl  -o "%(title)s-%(id)s.%(ext)s" $*'
