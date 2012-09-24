7c3397231af4893255713a210306b23305fe41e3
Geoffrey Huntley <ghuntley@ghuntley.com>
2012-09-24 18:43:58 +1000

    https://gist.github.com/3773804

--- python/env.zsh
+++ python/env.zsh
@@ -1,1 +1,7 @@
 export PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
+
+# https://gist.github.com/3773804
+export PATH=/usr/local/bin:/usr/local/share/python:$PATH
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/share/python/virtualenvwrapper.sh
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
