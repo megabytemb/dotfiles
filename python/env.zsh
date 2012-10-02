e0e3a76e22376cb9d273778fdfbf75fa2274ff89
Geoffrey Huntley <ghuntley@ghuntley.com>
2012-10-02 14:25:15 +1000

    source only if the script exists

--- python/env.zsh
+++ python/env.zsh
@@ -3,5 +3,8 @@ export PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
 # https://gist.github.com/3773804
 export PATH=/usr/local/bin:/usr/local/share/python:$PATH
 export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/share/python/virtualenvwrapper.sh
+if [ -f /usr/local/share/python/virtualenvwrapper.sh ] 
+then
+    source /usr/local/share/python/virtualenvwrapper.sh
+fi
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
