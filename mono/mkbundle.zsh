2b6aa4db68130aeeb8ad9da5867694d0218babae
Michael Benz <michael@benz.io>
2015-02-25 08:52:56 +1100

    fix CC

--- mono/mkbundle.zsh
+++ mono/mkbundle.zsh
@@ -1,4 +1,4 @@
 # mkbundle see -> http://stackoverflow.com/questions/13979044/mkbundle-on-mac-with-mono-mono-metadata-mono-config-h-file-not-found
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
 export AS="as -arch i386"
-export CC="clang -arch i386 -mmacosx-version-min=10.6"
+#export CC="clang -arch i386 -mmacosx-version-min=10.6"
