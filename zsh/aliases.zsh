e39706425a9140b1530d735edc3e940f0ea1e27a
Michael Benz <michael@benz.io>
2015-02-25 08:55:35 +1100

    mmmm Chromium

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,15 @@ alias dotcd='cd $ZSH'
 alias ping='ping -c 5' 
 
 alias reload!='. ~/.zshrc'
+
+if [[ $platform == 'osx' ]]; then
+	export PATH="$PATH":~/Code/depot_tools
+	export GYP_GENERATORS=ninja
+	export GYP_DEFINES='component=shared_library'
+	# This shows remaining/total build files.
+	export NINJA_STATUS="[%u/%t] "
+
+	alias make_chromium="time caffeinate ninja -C ~/Code/chromium/src/out/Release chrome"
+	alias go_chromium="~/Code/chromium/src/out/Release/Chromium.app/Contents/MacOS/Chromium --no-default-browser-check --disable-hang-monitor"
+	alias tests="time caffeinate ninja -C ~/Code/chromium/src/out/Release unit_tests browser_tests"
+fi
