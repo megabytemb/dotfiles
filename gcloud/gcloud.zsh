366eb534886dc7e3d2e94d3d34e481214bb58301
Michael Benz <michael@benz.io>
2015-06-01 23:22:38 +0000

    Added GCloud

---
+++ gcloud/gcloud.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+source '/home/michael/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables bash completion for gcloud.
+source '/home/michael/google-cloud-sdk/completion.zsh.inc'
